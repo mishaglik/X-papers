2d65ec4ac31ec61d9b90172943f535d8dba5f438
mishaglik <toomicle@gmail.com>
2023-04-05 16:39:24 +0300

    Fix santize flags

--- cmake/Sanitize.cmake
+++ cmake/Sanitize.cmake
@@ -14,11 +14,11 @@ if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     add_compile_options(${ASAN_COMPILE_FLAGS})
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ASAN_LINK_FLAGS}")
     
-    set(TSAN_COMPILE_FLAGS -fsanitize=thread -fno-sanitize-recover=all)
-    set(TSAN_LINK_FLAGS -fsanitize=thread)
-    message(STATUS "Sanitize with Thread Sanitizer (TSAN)")
-    add_compile_options(${TSAN_COMPILE_FLAGS})
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${TSAN_LINK_FLAGS}")
+    # set(TSAN_COMPILE_FLAGS -fsanitize=thread -fno-sanitize-recover=all)
+    # set(TSAN_LINK_FLAGS -fsanitize=thread)
+    # message(STATUS "Sanitize with Thread Sanitizer (TSAN)")
+    # add_compile_options(${TSAN_COMPILE_FLAGS})
+    # set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${TSAN_LINK_FLAGS}")
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     set (GNU_SANITIZE_FLAGS
     -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr
