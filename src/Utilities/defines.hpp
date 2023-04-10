a5e8428d202627dbcd19865e71128533388087b6
mishaglik <toomicle@gmail.com>
2023-04-10 11:31:59 +0300

    Add file structure

---
+++ src/Utilities/defines.hpp
@@ -0,0 +1,17 @@
+
+namespace xppr {
+    using byte_t = char;
+
+#ifndef NDEBUG
+#define IFDEBUG(...) __VA_ARGS__
+#else
+#define IFDEBUG(...)
+#endif
+
+// timestamp: [ERROR] module: 
+
+log ("F returned error {}", error);
+
+enum class EngineError
+{}; 
+}
