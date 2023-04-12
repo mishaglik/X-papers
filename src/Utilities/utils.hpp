91ac5df70a72b6c4a3056c71f4ba40d61fa9d9f6
mishaglik <toomicle@gmail.com>
2023-04-12 21:47:58 +0300

    Add Optional

--- src/Utilities/utils.hpp
+++ src/Utilities/utils.hpp
@@ -1,7 +1,7 @@
 #ifndef UTILITIES_UTILS_HPP
 #define UTILITIES_UTILS_HPP
 
-#include "log.hpp"
-#include "defines.hpp"
-
+#include <Utilities/log.hpp>
+#include <Utilities/defines.hpp>
+#include <Utilities/optional.hpp>
 #endif /* UTILITIES_UTILS_HPP */
