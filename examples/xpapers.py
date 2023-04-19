22f30443af8f73a8e656121809858f30da82aca4
mishaglik <toomicle@gmail.com>
2023-04-19 16:50:50 +0300

    Update sample

--- examples/xpapers.py
+++ examples/xpapers.py
@@ -9,17 +9,18 @@ xppr.loadPlugin("plugins/Clocks.so") # Loads plugin and simunaniously update pyt
 
 clock = xppr.Clocks.addClock("Digit clock", .posX=middle, .posY=0)
 
-if animation = xppr.loadAnimationFromShader("Aboba.gsls") :
+if animation = xppr.loadAnimationFromShader("Aboba.gsls"):
   xppr.setBackroundImageChangeAnimation(animation)
 else:
   xppr.setBackroundImageChangeAnimation(xppr.Animations[1])
 xppr.setBackroundImageChangeTrigger(clock.every(60))
 
 xppr.loadPlugin("plugins/Neofetch")
-xppr.Neofetch.addNeofetch("/* Args to neofetch */")
+neo = xppr.Neofetch.addNeofetch("/* Args to neofetch */")
+neo.setPosition(1000, 100)
 
 label = xppr.CreateLabel(0, 100, 100, 10)
 label.setFont("Fira Mono")
 label.setTextStyle("Italic")
-label.setText("CPU: {}%", xppr.getCpuLoad)
+label.setText("CPU: {}%", xppr.getCpuLoad())
 label.show()
