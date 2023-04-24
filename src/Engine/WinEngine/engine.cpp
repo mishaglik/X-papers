9781b4833783f3ef093c01baf8973a46d7a1fa4b
Execorn <n.biryukov.04@gmail.com>
2023-04-24 09:33:36 +0300

    X handler (#2)
    * .test folder added
    
    * event handler and var handler added
    
    * c_str() replaced with data() to avoid const_cast
    
    * milliseconds removed from log
    
    * formatting settings added
    
    * xmanager + xwindowhandler implemented
    
    * old files removed
    
    * fixes constexpr
    
    * cpp files added, fixes
    
    * prototypes added
    
    * simple pair_impl class added
    
    * full rework + mvp
    
    * test added
    
    * removed binary
    
    * newlines added to avoid warnings
    
    * fixes, now stable

--- src/Engine/WinEngine/engine.cpp
+++ src/Engine/WinEngine/engine.cpp
@@ -1,10 +1,40 @@
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xos.h>
-
 #include "engine.hpp"
+#include "WinEngine/proto.hpp"
+#include "XManager.hpp"
 
-void test_open_window()
-{
+void test_open_window() {
+    winengine::win_attr_t attrib;
+    attrib.override_redirect = True;
 
+    auto manager = winengine::XWindowManager::getInstance();
+    auto win = manager->addWindow({0, 0}, {1920, 1080}, 0, winengine::Parent,
+                                  CopyFromParent, 0, &attrib);
+
+    auto wintype = manager->createAtom("_NET_WM_WINDOW_TYPE", False);
+    auto desktop = manager->createAtom("_NET_WM_WINDOW_TYPE_DESKTOP", False);
+
+    win->changeProperty(wintype, XA_ATOM, 32, PropModeReplace,
+                        (unsigned char*)&desktop, 1);
+
+    auto wm_state = manager->createAtom("_NET_WM_STATE", true);
+    auto wm_fullscreen = manager->createAtom("_NET_WM_STATE_FULLSCREEN", true);
+
+    win->changeProperty(wm_state, XA_ATOM, 32, PropModeReplace,
+                        (unsigned char*)&wm_fullscreen, 1);
+
+    win->setBackground(0xFFFFFF);
+
+    win->clear();
+    win->show();
+
+    while (1) {
+        XEvent event;
+
+        manager->nextEvent(&event);
+    }
+
+    win->hide();
+    manager->closeDisplay();
+
+    return;
 }
--- src/Utilities/optional.hpp
+++ src/Utilities/optional.hpp
@@ -9,114 +9,137 @@
 #include "defines.hpp"
 namespace xppr {
 
+template <typename T, typename Err, Err NoErr = 0, Err ErrHasNoValue = 1>
+    requires std::destructible<T> && std::is_scalar_v<Err> &&
+             requires { NoErr != ErrHasNoValue; }
+class Optional {
+   public:
+    // Error = Default constructor
+    /*implicit*/ constexpr Optional(Err err = ErrHasNoValue) noexcept
+        : m_err(err) {
+        assert(err != NoErr);
+    }
+    /*implicit*/ constexpr Optional& operator=(Err err) noexcept {
+        assert(err != NoErr);
+        reset();
+        m_err = (err == NoErr) ? ErrHasNoValue : err;
+    }
 
-    template<typename T, typename Err, Err NoErr = 0, Err ErrHasNoValue = 1>
-    requires std::destructible<T> && std::is_scalar_v<Err> && requires{ NoErr != ErrHasNoValue;}
-    class Optional
-    {
-    public:
-        // Error = Default constructor
-        /*implicit*/constexpr Optional(Err err = ErrHasNoValue) noexcept : m_err(err) { assert(err != NoErr); }
-        /*implicit*/constexpr Optional& operator=(Err err) noexcept { 
-            assert(err != NoErr); 
-            reset();
-            m_err = (err == NoErr) ? ErrHasNoValue : err; 
+    // Value copy constructor
+    /*implicit*/ constexpr Optional(const T& t) noexcept : m_err(NoErr) {
+        new (m_data) T(t);
+    }
+    constexpr Optional& operator=(const T& t) noexcept {
+        reset();
+        new (m_data) T(t);
+        m_err = NoErr;
+    }
+
+    // Value move constructor
+    /*implicit*/ constexpr Optional(T&& t) noexcept : m_err(NoErr) {
+        new (m_data) T(std::move(t));
+    }
+    constexpr Optional& operator=(T&& t) noexcept {
+        reset();
+        new (m_data) T(std::move(t));
+        m_err = NoErr;
+    }
+
+    // Copy constructor
+    constexpr Optional(const Optional& oth) noexcept : m_err(oth.m_err) {
+        if (oth) {
+            new (m_data) T(*oth);
         }
-
-        // Value copy constructor
-        /*implicit*/constexpr Optional(const T& t) noexcept : m_err(NoErr) { new(m_data) T(t); }
-        constexpr Optional& operator= (const T& t) noexcept {
-            reset();
-            new(m_data) T(t);
-            m_err = NoErr;
+    }
+    constexpr Optional& operator=(const Optional& oth) noexcept {
+        reset();
+        m_err = oth.m_err;
+        if (oth) {
+            new (m_data) T(*oth);
         }
+    }
 
-        // Value move constructor
-        /*implicit*/constexpr Optional(T&& t) noexcept : m_err(NoErr) { new(m_data) T(std::move(t)); }
-        constexpr Optional& operator=(T&& t)  noexcept {
-            reset();
-            new(m_data) T(std::move(t)); 
-            m_err = NoErr;
+    // Move constructor
+    constexpr Optional(Optional&& oth) noexcept : m_err(oth.m_err) {
+        if (oth) {
+            new (m_data) T(std::move(*oth));
+            oth.m_err = ErrHasNoValue;
         }
+    }
 
-        // Copy constructor
-        constexpr Optional(const Optional& oth) noexcept : m_err(oth.m_err) { if(oth){ new(m_data) T(*oth);} }
-        constexpr Optional& operator=(const Optional& oth) noexcept { 
-            reset();
-            m_err = oth.m_err;
-            if(oth){ 
-                new(m_data) T(*oth);
-            } 
+    constexpr Optional& operator=(Optional&& oth) noexcept {
+        reset();
+        m_err = oth.m_err;
+        if (oth) {
+            new (m_data) T(*oth);
+            oth.m_err = ErrHasNoValue;
         }
+    }
 
-        // Move constructor
-        constexpr Optional(Optional&& oth) noexcept : m_err(oth.m_err) { 
-            if(oth) { 
-                new(m_data) T(std::move(*oth));
-                oth.m_err = ErrHasNoValue;
-            } 
+    // Emplace constructor
+    template <class... Args>
+    constexpr Optional(Args&&... args) {
+        emplace(std::forward<Args>(args)...);
+    }
+
+    constexpr ~Optional() { reset(); }
+
+    template <class... Args>
+    constexpr T& emplace(Args&&... args) {
+        reset();
+        new (m_data) T(std::forward<Args>(args)...);
+        m_err = NoErr;
+    }
+
+    constexpr void reset() noexcept {
+        if (hasValue()) {
+            value().T::~T();
         }
+    }
 
-        constexpr Optional& operator=(Optional&& oth) noexcept { 
-            reset();
-            m_err = oth.m_err;
-            if(oth){ 
-                new(m_data) T(*oth);
-                oth.m_err = ErrHasNoValue;
-            } 
-        }
+    constexpr T& operator*() {
+        requireValue();
+        return *reinterpret_cast<T*>(m_data);
+    }
+    constexpr const T& operator*() const {
+        requireValue();
+        return *reinterpret_cast<const T*>(m_data);
+    }
 
-        //Emplace constructor 
-        template<class... Args >
-        constexpr Optional(Args&&... args ) { emplace(std::forward<Args>(args)...); }
+    constexpr T* operator->() {
+        requireValue();
+        return reinterpret_cast<T*>(m_data);
+    }
+    constexpr const T* operator->() const {
+        requireValue();
+        return reinterpret_cast<const T*>(m_data);
+    }
 
-        constexpr ~Optional() { reset(); }
-        
+    constexpr bool hasValue() const noexcept { return m_err == NoErr; }
+    constexpr operator bool() const noexcept { return hasValue(); }
 
-        template<class... Args >
-        constexpr T& emplace(Args&&... args ) {
-            reset();
-            new(m_data) T(std::forward<Args>(args)...);
-            m_err = NoErr;
+    constexpr T& value() { return **this; }
+    constexpr const T& value() const { return **this; }
 
-        }
+    constexpr Err error() const { return m_err; }
 
-        constexpr void reset() noexcept {
-            if(hasValue()) {
-                value().T::~T();
-            }
-        }
+    constexpr std::strong_ordering operator<=>(Err err) const noexcept {
+        return m_err <=> err;
+    }
 
-        constexpr       T& operator*()        { requireValue(); return *reinterpret_cast<      T*>(m_data); }
-        constexpr const T& operator*()  const { requireValue(); return *reinterpret_cast<const T*>(m_data); }
+   private:
+    /**
+     * @brief Needed to be destructible if T has no trivial destructor;
+     */
+    byte_t m_data[sizeof(T)];
+    Err m_err = ErrHasNoValue;
 
-        constexpr T*       operator->()       { requireValue(); return  reinterpret_cast<      T*>(m_data); }
-        constexpr const T* operator->() const { requireValue(); return  reinterpret_cast<const T*>(m_data); }
-
-        constexpr bool hasValue() const noexcept {return m_err == NoErr;}
-        constexpr operator bool() const noexcept {return hasValue();}
-
-        constexpr       T& value()        {return **this;}
-        constexpr const T& value() const  {return **this;}
-
-        constexpr Err error() const {return m_err;}
-
-
-        constexpr std::strong_ordering operator<=>(Err err) const noexcept { return m_err <=> err; }
-
-    private:
-        /**
-         * @brief Needed to be destructible if T has no trivial destructor;   
-         */
-        byte_t m_data[sizeof(T)];  
-        Err m_err = ErrHasNoValue;
-
-        constexpr void requireValue() const {
-            //FIXME: Abortion or throw required.
-            abort();
-            // throw ErrHasNoValue;
-        }
-    };
-}
+    constexpr void requireValue() const {
+        // FIXME: Abortion or throw required.
+        abort();
+        // throw ErrHasNoValue;
+    }
+};
+}  // namespace xppr
 
 #endif /* UTILITIES_OPTIONAL_HPP */
