42e00a49936a8da5d44e4a187daf6e008e9c7edb
execorn <n.biryukov.04@gmail.com>
2023-04-29 01:33:38 +0300

    testing

--- src/Engine/WinEngine/engine.cpp
+++ src/Engine/WinEngine/engine.cpp
@@ -1,12 +1,19 @@
 #include "engine.hpp"
+#include <iostream>
 #include "WinEngine/proto.hpp"
-#include "XManager.hpp"
+#include "WindowEngine.hpp"
+#include "XDisplayHandler.hpp"
 
 void test_open_window() {
+    auto engine = winengine::WindowEngine::getInstance();
+    for (std::size_t i = 0; i < engine.getTotalDisplays(); ++i) {
+        std::cout << engine[i].getScreenCount() << std::endl;
+    }
+
     winengine::win_attr_t attrib;
     attrib.override_redirect = True;
 
-    auto manager = winengine::XWindowManager::getInstance();
+    auto manager = winengine::XDisplayHandler::getInstance();
     auto win = manager->addWindow({0, 0}, {1920, 1080}, 0, winengine::Parent,
                                   CopyFromParent, 0, &attrib);
 
@@ -16,13 +23,14 @@ void test_open_window() {
     win->changeProperty(wintype, XA_ATOM, 32, PropModeReplace,
                         (unsigned char*)&desktop, 1);
 
-    auto wm_state = manager->createAtom("_NET_WM_STATE", true);
-    auto wm_fullscreen = manager->createAtom("_NET_WM_STATE_FULLSCREEN", true);
+    // auto wm_state = manager->createAtom("_NET_WM_STATE", true);
+    // auto wm_fullscreen = manager->createAtom("_NET_WM_STATE_FULLSCREEN",
+    // true);
 
-    win->changeProperty(wm_state, XA_ATOM, 32, PropModeReplace,
-                        (unsigned char*)&wm_fullscreen, 1);
+    // win->changeProperty(wm_state, XA_ATOM, 32, PropModeReplace,
+    //                     (unsigned char*)&wm_fullscreen, 1);
 
-    win->setBackground(0xFFFFFF);
+    win->setBackground(0xFFFFFF);  // { width, heigth }
 
     win->clear();
     win->show();
