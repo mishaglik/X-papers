05ad393ba8c5c051168e467e8e83ca167454707f
mishaglik <toomicle@gmail.com>
2023-05-22 03:36:41 +0300

    Link Movie

--- src/Plugins/Widget/Movie/Movie.cpp
+++ src/Plugins/Widget/Movie/Movie.cpp
@@ -48,3 +48,36 @@ void Movie::fit(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
     m_renderer.fit(static_cast<float>(x), static_cast<float>(y), static_cast<float>(width), static_cast<float>(height));
     m_renderer.play();
 }
+
+extern const xppr::meta::MetaType MovieMgrType;
+
+struct MovieMgr : xppr::meta::MetaObjectT<&MovieMgrType> {
+    xppr::ApplicationAPI m_api;
+    xppr::meta::MetaObject* add(uint64_t i);
+};
+
+xppr::meta::MetaObject* MovieMgr::add(uint64_t i) {
+    if(i >= m_api.NDisplays()) {
+        return new xppr::meta::MetaError{{}, xppr::meta::MetaError::Type::AttributeError, "index of display is too big!"};
+    }
+    auto* bg = new Movie();
+    m_api.addWidget(bg, i);
+    return bg;
+}
+
+const xppr::meta::MetaFuction MovieMgrMeths[] {
+    META_METHOD(MovieMgr, add),
+    {nullptr, nullptr, nullptr}
+};
+
+const xppr::meta::MetaType MovieMgrType {
+    .name = "movie",
+    .methods = MovieMgrMeths,
+    .members = nullptr, 
+    .dtor = nullptr
+};
+
+extern "C" void init_plugin(xppr::ApplicationAPI api) { 
+    xppr::log::add_logger();
+    api.registerObject(new MovieMgr{{}, api});
+}
